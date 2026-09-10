75a91ef55ce68d4c76fdb3ec0e5924a7477f3ee1
Daninkk <flamezueiro@gmail.com>
2026-09-10 18:54:54 -0300

    feat: adicao da landingpage

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import TelaGarcom from './pages/TelaGarcom';
 import TelaCozinha from './pages/TelaCozinha';
 import TelaAdministrador from './pages/TelaAdministrador';
+import LandingPage from './pages/LandingPage';
 
 function App(){
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TelaLogin />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<TelaLogin />} />
         <Route path="/garcom" element={<TelaGarcom />} />
         <Route path="/cozinha" element={<TelaCozinha />} />
         <Route path="/administrativo" element={<TelaAdministrador />} />
